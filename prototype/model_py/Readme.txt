faaa4e4cf93e11e986d880deefa9ba9e6fdebc30
Marek Makowski <marek@iiasa.ac.at>
2021-08-07 21:20:00 +0200

    msg01.py modified and tested
    Initial Python code modified and tested. See Readme.txt for details and log01.txt for results of the test run. Suggestions for further modifications are in the msg0jz.py

--- prototype/model_py/Readme.txt
+++ prototype/model_py/Readme.txt
@@ -1,3 +1,10 @@
 This folder shall be used for the Python code used for generating the elements
-of the model. It is initialized with msg1.py, which was copied from the ptl_jz
-repo, and later modified to use relocated and renamed data files.
+of the model. It is initialized with msg01.py, which was copied from the ptl_jz
+repo, and later modified to use the relocated and renamed data files.
+
+Please add to  the .gitignore file (or create such a file in this dir) the line:
+.idea
+This is necessary to refrain from committing the local pycharm environment to the repo.
+
+The first commit contains copy of msg01.py named msg01jz.py to be used by Jinyang
+for further modifications.
---
+++ prototype/model_py/msg01.py
@@ -0,0 +1,70 @@
+"""
+This code is based on the jupyter nb by Jinyang; info on cells was removed.
+The code was modified by Marek to read data from a dedicated directory and from renamed (to use short names) files.
+Code was also modified to conform to the PEP 8 (i.e., all pycharm warnings were addressed).
+see the Readme.txt for more  info
+
+The code reports two errors (on import ixmp and message+ix) but runs despite the reported errors.
+Open issues:
+- optimization finished OK; the results needs to be checked.
+- post-processing reports two errors, both related to the gdx content
+The console output is stored in log01.txt file.
+
+NOTE: the GAMS files are located message_ix installation dependent dir. In Marek's iMac:
+/Users/marek/anaconda3/envs/msg_env/lib/python3.9/site-packages/message_ix/model/
+It would be good to ind out how to change this (default?) location.
+"""
+
+# the following two lines should not be needed in pycharm:
+#   #!/usr/bin/env python
+# # coding: utf-8
+
+# import pandas as pd  (not needed yet)
+# The following two imports result in errors ("No module named ixmp") but the code works despite the reported errors
+import ixmp
+import message_ix
+
+# the following commented because in pycharm it gives error
+# get_ipython().run_line_magic('matplotlib', 'inline')
+
+# Loading Modeling platform
+mp = ixmp.Platform()
+#  if the DB is locked then remove ~/.local/share/ixmp/localdb/default.lck
+
+# Creating a new, empty scenario
+scenario = message_ix.Scenario(mp, model='PTL baseline', scenario='baseline_xlsx', version='new')
+
+horizon = range(2020, 2051, 5)
+scenario.add_horizon(year=horizon)
+
+node = 'Chinese'
+scenario.add_spatial_sets({'country': node})
+scenario.add_set("commodity", ["coal", "oil", "hydrogen", "CO2", "biomass", "gasoline", "diesel", "liquid"])
+scenario.add_par("interestrate", horizon, value=0.06, unit='-')
+scenario.add_set("level", ["primary", "secondary", "final", "useful"])
+
+data_dir = '../data/'   # directory the data files
+scenario.read_excel(data_dir + 'demand.xlsx', add_units=True, commit_steps=False)
+print(scenario.idx_names('demand'))
+
+scenario.add_set("technology", ['oil_exc', 'oil_import', 'coal_exc', 'renewable_hydrogen', 'CO2_capture',
+                 'biomass_exc', 'OTL', 'CTL', 'PTL', 'BTL', 'Transportation'])
+scenario.read_excel(data_dir + 'basic.xlsx', add_units=True, commit_steps=False)
+scenario.set('technology')
+scenario.par('capacity_factor')
+scenario.read_excel(data_dir + 'constraint.xlsx', add_units=True, commit_steps=False)
+scenario.read_excel(data_dir + 'historic.xlsx', add_units=True, commit_steps=False)
+
+mp.add_unit('CNY/t')
+scenario.read_excel(data_dir + 'economic.xlsx', add_units=True, commit_steps=False)
+
+scenario.set_as_default()
+scenario.solve()
+
+# The following 3 cmds results in errors (probably because these scenarios are unknown on imac
+# Objective value of the original 'baseline' scenario.
+# base = message_ix.Scenario(mp, model='PTL model attempt', scenario='baseline')
+# base.var('OBJ')['lvl']
+# scenario.var('OBJ')['lvl']
+
+mp.close_db()
