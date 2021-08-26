d3883233c3126c08e850e327f973459a86373e02
Marek Makowski <marek@iiasa.ac.at>
2021-08-26 08:28:34 +0200

    Editorial improvements of outp01.py
    Comments improved; causes of warnings removed.

--- prototype/model_py/outp01.py
+++ prototype/model_py/outp01.py
@@ -3,8 +3,8 @@ Read the gdx files containg the solutions of the MCMA iterations defined by itr_
 Extract the values of ACT variables (activity levels) for the technologies defined by tec list and
 their values together with the corresponding subset of indices and values defined by df_cols list.
 Store the extracted values with the add itr_id in the act.csv file.
-The *gdx files and the act.csv file are located in the directory 945 (the number indicates the MCMA analysis id
-to which all iterations belong).
+The *gdx files and the act.csv file are located in the directory named 945 (the number indicates the MCMA
+analysis_id to which all these iterations belong).
 """
 import gdxpds
 # import pandas as pd  not needed (probably imported with gdxpds)
@@ -16,9 +16,9 @@ for itr_id in itr_lst:
     gdx_file = '945/' + str(itr_id) + '.gdx'
     dataframes = gdxpds.to_dataframes(gdx_file)
 
-    # select values of ACT for the subset of technologies defined by tec
-    df_cols = ['tec', 'vintage', 'year_all', 'Level']
-    tec = ['OTL', 'CTL', 'PTL', 'BTL']
+    # extract subset of ACT (compound variable representiving activities) values
+    df_cols = ['tec', 'vintage', 'year_all', 'Level']   # subset of attributes/columns to be extracted
+    tec = ['OTL', 'CTL', 'PTL', 'BTL']      # subset of technologies to be extracted
     for symbol_name, df in dataframes.items():
         if symbol_name == 'ACT':
             df_act = df.loc[df['tec'].isin(tec), df_cols]
@@ -26,9 +26,9 @@ for itr_id in itr_lst:
             if itr_id == itr_lst[0]:    # for the first iter: write csv header, initialize the csv file
                 df_act.to_csv(fn_csv, index=False, float_format='%.3e', mode='w')
             else:       # for subsequent iters: don't write header, add rows to those previously written
-                df_act.to_csv(fn_csv, index=False, float_format='%.3e', mode='a', header = False)
+                df_act.to_csv(fn_csv, index=False, float_format='%.3e', mode='a', header=False)
             # print("Selected columns and rows of {} stored in df_act.".format(symbol_name))
-            print('Values of selected columns and rows of ACT of iter ' + str(itr_id)  + ' added to act.csv file.')
+            print('Values of selected columns and rows of ACT of iter ' + str(itr_id) + ' added to act.csv file.')
             break
 
 print('Finished writing the act.csv file.')
