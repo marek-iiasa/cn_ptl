63232dae86483a2dd9d5e6d46d72e122204d34b3
Marek Makowski <marek@iiasa.ac.at>
2022-02-02 20:22:59 +0100

    outp01.py advanced
    Improved processing of results; not finished yet.

--- prototype/model_py/outp01.py
+++ prototype/model_py/outp01.py
@@ -3,8 +3,8 @@ Read the gdx files containg the solutions of the MCMA iterations defined by itr_
 Extract the values of ACT variables (activity levels) for the technologies defined by tec list and
 their values together with the corresponding subset of indices and values defined by df_cols list.
 Store the extracted values with the add itr_id in the act.csv file.
-The *gdx files and the act.csv file are located in the directory named 945 (the number indicates the MCMA
-analysis_id to which all these iterations belong).
+The *gdx files and the act.csv file are located in the directory named after the corresponding MCMA
+analysis_id to which all these iterations belong.
 """
 import gdxpds   # warning is displayed, if this is not the first import
 import sys
@@ -14,27 +14,45 @@ import pandas as pd
 # import pathlib
 sns.set()   # default settings for seaborn plotting style
 
-mk_csv = False
-fn_csv = '945/act.csv'      # csv file with values of ACT from the itr_lst
-itr_lst = range(2121, 2144)
-# itr_lst = range(2121, 2124)     # 2124 replaced by 2122 for testing just 3 iters for testing
-# itr_lst = [2122, 2123, 2124, 2143, 2134, 2131, 2129]  # subset showing non-monotonicity of CO2
-tec = ['OTL', 'CTL', 'PTL', 'BTL']  # subset of technologies to be extracted
+mk_csv = False      # if True only ACT csv if written
+dir_dat = '985'    # scaled params, correct Pareto solutions
+# dir_dat = '945'    # not scaled params, wrong Pareto solutions, i.e., some are dominated
+fn_csv = dir_dat + '/act.csv'      # csv file with values of ACT from the itr_lst
+
+zero_tol = 1e-6     # smaller Levels assumed to be equal to zero
+#  tec = ['OTL', 'CTL', 'PTL', 'BTL']  # subset of technologies to be extracted
+tec = ['OTL', 'PTL']  # subset of technologies to be extracted (ACT == 0 for CTL & BTL)
+# itr_lst = []
+if dir_dat == '985':
+    scal_co2_cost = 1.  # no need for scaling cost and CO2
+    scal_act = 1.
+    # itr_lst = [2303, 2333, 2322, 2302]    # two selfish, and two "in between"
+    itr_lst = [2304]    # compromise solution
+else:
+    scal_co2_cost = 1.e-9  # scale cost and CO2
+    scal_act = 1.e-9
+    itr_lst = range(2121, 2144)
+    # itr_lst = range(2121, 2124)     # 2124 replaced by 2122 for testing just 3 iters for testing
+    # itr_lst = [2122, 2123, 2124, 2143, 2134, 2131, 2129]  # subset showing non-monotonicity of CO2
+
 small_dfs = []      # list of ACT dfs, each for one iter
 itr_co2 = itr_cost = -1.
-scal_co2_cost = 1.e-9   # scale cost and CO2
 row_lst = []
 for itr_id in itr_lst:
     i_found = 0
-    gdx_file = '945/' + str(itr_id) + '.gdx'
+    gdx_file = dir_dat + '/' + str(itr_id) + '.gdx'
     dataframes = gdxpds.to_dataframes(gdx_file)
 
-    # extract subset of ACT (compound variable representiving activities) values
+    # extract subset of ACT (compound variable representing activities) values
     df_cols = ['tec', 'vintage', 'year_all', 'Level']   # subset of attributes/columns to be extracted
     for symbol_name, df in dataframes.items():
         if symbol_name == 'ACT':
             i_found += 1
-            df_act = df.loc[df['tec'].isin(tec), df_cols]   # select only rows with techn in tec[]
+            df_act1 = df.loc[df['tec'].isin(tec), df_cols]   # select only rows with techn in tec[]
+            # only rows with techn in tec[] and non-zero values of Level
+            df_act = df.loc[(df['tec'].isin(tec)) & (df['Level'] > zero_tol), df_cols]
+            print('Non-zero and all values of levels of considered technologies: ', str(len(df_act)), ', ',
+                  str(len(df_act1)))
             df_act.insert(0, 'iter', str(itr_id))   # add column with the itr_id
             if mk_csv:
                 if itr_id == itr_lst[0]:    # for the first iter: write csv header, initialize the csv file
@@ -67,15 +85,21 @@ df_cost_co2 = pd.DataFrame(row_lst, columns=['itr', 'co2', 'cost'])
 cost_co2 = df_cost_co2.sort_values(by='cost')
 act_all = pd.concat(small_dfs, ignore_index=True)
 
-# act_tmp = act_all.loc[(act_all['tec'] == 'OTL'), 'Level']
-# act_tmp2 = act_all.loc[(act_all['year_all'] == str(2025)), 'Level']
-# act_tmp3 = act_all.loc[(act_all['tec'] == 'OTL') & (act_all['year_all'] == str(2025)), 'Level']
-# sum_by_vintage = act_all.loc[(act_all['tec'] == 'OTL') & (act_all['year_all'] == str(2025)), 'Level'].sum()
-# print(act_tmp3)
+print('cost_co2')
+print(cost_co2)
+
+print('act_all')
+print(act_all)
+
+act_tmp = act_all.loc[(act_all['tec'] == 'OTL'), 'Level']
+act_tmp2 = act_all.loc[(act_all['year_all'] == str(2025)), 'Level']
+act_tmp3 = act_all.loc[(act_all['tec'] == 'OTL') & (act_all['year_all'] == str(2025)), 'Level']
+sum_by_vintage = act_all.loc[(act_all['tec'] == 'OTL') & (act_all['year_all'] == str(2025)), 'Level'].sum()
+print('act_tmp3')
+print(act_tmp3)
 print('Collected ACT values of all iters into act_all')
 
 periods = range(2020, 2055, 5)  # starting year of each of 5-yr period
-scal_act = 1.e-9
 for itr in itr_lst:
     itr_sum = 0.
     sum_by_vintage = 0.
@@ -90,6 +114,10 @@ for itr in itr_lst:
             itr_sum = round(itr_sum, 3)
     print('Sum of all activities of itr ' + str(itr) + ': ' + str(itr_sum))
 print('Sums of ACT by vintage, calculated for each techn and period (to be stored in a df for charts).')
+
+if len(itr_lst) < 2:      # don't make chart for only one itr
+    print('Chart not generated for only one iteration.')
+    sys.exit(0)
 
 # plot CO2/cost trade-offs chart
 fig1 = plt.figure(figsize=(10, 5))
@@ -110,7 +138,7 @@ ax.legend(loc='center right', handles=hand1+hand2, labels=lab1+lab2)
 ax2.legend(loc='center right', handles=hand1+hand2, labels=lab1+lab2)   # overlap prevents aditional legend for 2nd plot
 
 plt.show()
-fig_name = '945/cost_co2.png'
+fig_name = dir_dat + '/cost_co2.png'
 # fig1.savefig(fig_name)
 # print('Figure stored in ' + fig_name)
 print('Figure NOT stored in ' + fig_name)
